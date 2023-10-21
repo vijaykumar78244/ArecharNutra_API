72eb765fdc0d66c211a486d29d7aead2bdbd6273
vijaykumar78244 <147585017+vijaykumar78244@users.noreply.github.com>
2023-10-21 10:58:17 +0530

    Update views.py

--- arechar_nutra/views.py
+++ arechar_nutra/views.py
@@ -5,11 +5,24 @@ from .models import *
 from .serializers import *
 
 
+
+
 class WhoAreWeList(APIView):
     def get(self, request):
         who_are_we_list = WhoAreWeModel.objects.all()
-        serializer = WhoAreWeSerializer(who_are_we_list, many=True)
-        return Response(serializer.data)
+        who_are_we_data = WhoAreWeSerializer(who_are_we_list, many=True).data
+
+        response_data = {
+            "response_code": "success",
+            "http_status_code": 200,
+            "context": {
+                "success": {
+                    "who_are_we_list": who_are_we_data
+                }
+            },
+            "message": "OK",
+        }
+        return Response(response_data)
 
 class WhoAreWeDetail(APIView):
     def get_object(self, pk):
@@ -19,17 +32,42 @@ class WhoAreWeDetail(APIView):
             raise Http404
 
     def get(self, request, pk):
-        who_are_we_details = self.get_object(pk)
-        serializer = WhoAreWeSerializer(who_are_we_details)
-        return Response(serializer.data)
+        try:
+            who_are_we_details = self.get_object(pk)
+            who_are_we_data = WhoAreWeSerializer(who_are_we_details).data
 
+            response_data = {
+                "response_code": "success",
+                "http_status_code": 200,
+                "context": {
+                    "success": {
+                        "who_are_we_detail": who_are_we_data
+                    }
+                },
+                "message": "OK",
+            }
+
+            return Response(response_data)
+        except WhoAreWeModel.DoesNotExist:
+            return Response({"error": "Who Are We data not found."}, status=404)
 
 class CertificationList(APIView):
     def get(self, request):
         certifications = CertificationModel.objects.all()
-        serializer = CertificationSerializer(certifications, many=True)
-        return Response(serializer.data)
+        certifications_data = CertificationSerializer(certifications, many=True).data
 
+        response_data = {
+            "response_code": "success",
+            "http_status_code": 200,
+            "context": {
+                "success": {
+                    "certification_list": certifications_data
+                }
+            },
+            "message": "OK",
+        }
+
+        return Response(response_data)
 
 class CertificationDetail(APIView):
     def get_object(self, pk):
@@ -39,16 +77,42 @@ class CertificationDetail(APIView):
             raise Http404
 
     def get(self, request, pk):
-        certification = self.get_object(pk)
-        serializer = CertificationSerializer(certification)
-        return Response(serializer.data)
+        try:
+            certification = self.get_object(pk)
+            certification_data = CertificationSerializer(certification).data
 
+            response_data = {
+                "response_code": "success",
+                "http_status_code": 200,
+                "context": {
+                    "success": {
+                        "certification_detail": certification_data
+                    }
+                },
+                "message": "OK",
+            }
+
+            return Response(response_data)
+        except CertificationModel.DoesNotExist:
+            return Response({"error": "Certification data not found."}, status=404)
 
 class FeaturedImageList(APIView):
     def get(self, request):
         featured_images = FeaturedImageModel.objects.all()
-        serializer = FeaturedImageSerializer(featured_images, many=True)
-        return Response(serializer.data)
+        featured_images_data = FeaturedImageSerializer(featured_images, many=True).data
+
+        response_data = {
+            "response_code": "success",
+            "http_status_code": 200,
+            "context": {
+                "success": {
+                    "featured_image_list": featured_images_data
+                }
+            },
+            "message": "OK",
+        }
+
+        return Response(response_data)
 
 class FeaturedImageDetail(APIView):
     def get_object(self, pk):
@@ -58,15 +122,45 @@ class FeaturedImageDetail(APIView):
             raise Http404
 
     def get(self, request, pk):
-        featured_image = self.get_object(pk)
-        serializer = FeaturedImageSerializer(featured_image)
-        return Response(serializer.data)
+        try:
+            featured_image = self.get_object(pk)
+            featured_image_data = FeaturedImageSerializer(featured_image).data
+
+            response_data = {
+                "response_code": "success",
+                "http_status_code": 200,
+                "context": {
+                    "success": {
+                        "featured_image_detail": featured_image_data
+                    }
+                },
+                "message": "OK",
+            }
+
+            return Response(response_data)
+        except FeaturedImageModel.DoesNotExist:
+            return Response({"error": "Featured Image not found."}, status=404)
     
+
+
 class SocialResponsibilityList(APIView):
     def get(self, request):
         social_responsibility_list = SocialResponsibilityModel.objects.all()
-        serializer = SocialResponsibilitySerializer(social_responsibility_list, many=True)
-        return Response(serializer.data)
+        social_responsibility_data = SocialResponsibilitySerializer(social_responsibility_list, many=True).data
+
+        response_data = {
+            "response_code": "success",
+            "http_status_code": 200,
+            "context": {
+                "success": {
+                    "social_responsibility_list": social_responsibility_data
+                }
+            },
+            "message": "OK",
+        }
+        
+        return Response(response_data)
+
 
 class SocialResponsibilityDetail(APIView):
     def get_object(self, pk):
@@ -76,6 +170,25 @@ class SocialResponsibilityDetail(APIView):
             raise Http404
 
     def get(self, request, pk):
+        try:
+            social_responsibility = self.get_object(pk)
+            social_responsibility_data = SocialResponsibilitySerializer(social_responsibility).data
+            response_data = {
+                "response_code": "success",
+                "http_status_code": 200,
+                "context": {
+                    "success": {
+                        "social_responsibility": social_responsibility_data
+                    }
+                },
+                "message": "OK",
+            }
+            
+            return Response(response_data)
+        except SocialResponsibilityModel.DoesNotExist:
+            return Response({"error": "Social Responsibility data not found."}, status=404)
+
+    def get(self, request, pk):
         social_responsibility_details = self.get_object(pk)
         serializer = SocialResponsibilitySerializer(social_responsibility_details)
         return Response(serializer.data)
@@ -83,8 +196,20 @@ class SocialResponsibilityDetail(APIView):
 class SocialMediaURLList(APIView):
     def get(self, request):
         social_media_url_list = SocialMediaURLModel.objects.all()
-        serializer = SocialMediaURLSerializer(social_media_url_list, many=True)
-        return Response(serializer.data)
+        social_media_url_data = SocialMediaURLSerializer(social_media_url_list, many=True).data
+
+        response_data = {
+            "response_code": "success",
+            "http_status_code": 200,
+            "context": {
+                "success": {
+                    "social_media_url_list": social_media_url_data
+                }
+            },
+            "message": "OK",
+        }
+
+        return Response(response_data)
 
 class SocialMediaURLDetail(APIView):
     def get_object(self, pk):
@@ -94,8 +219,55 @@ class SocialMediaURLDetail(APIView):
             raise Http404
 
     def get(self, request, pk):
-        social_media_url_details = self.get_object(pk)
-        serializer = SocialMediaURLSerializer(social_media_url_details)
-        return Response(serializer.data)
+        try:
+            social_media_url = self.get_object(pk)
+            social_media_url_data = SocialMediaURLSerializer(social_media_url).data
+
+            response_data = {
+                "response_code": "success",
+                "http_status_code": 200,
+                "context": {
+                    "success": {
+                        "social_media_url_details": social_media_url_data
+                    }
+                },
+                "message": "OK",
+            }
+
+            return Response(response_data)
+        except SocialMediaURLModel.DoesNotExist:
+            return Response({"error": "Social Media URL not found."}, status=404)
+
+
+class NestedDataView(APIView):
+    def get(self, request):
+        who_are_we_data = WhoAreWeModel.objects.all()
+        certification_data = CertificationModel.objects.all()
+        featured_image_data = FeaturedImageModel.objects.all()
+        social_responsibility_data = SocialResponsibilityModel.objects.all()
+        social_media_url_data = SocialMediaURLModel.objects.all()
+
+        response_data = {
+            "response_code": "success",
+            "http_status_code": 200,
+            "context": {
+                "success": {
+                    "WhoAreWeModel_data": WhoAreWeSerializer(who_are_we_data, many=True).data,
+                    "Certification_data": CertificationSerializer(certification_data, many=True).data,
+                    "FeaturedImage_data": FeaturedImageSerializer(featured_image_data, many=True).data,
+                    "SocialResponsibility_data": SocialResponsibilitySerializer(social_responsibility_data, many=True).data,
+                    "SocialMediaURL_data": SocialMediaURLSerializer(social_media_url_data, many=True).data,
+                }
+            },
+            "message": "OK",
+        }
+        return Response(response_data)
+
+
+
+
+
+
+
 
 
